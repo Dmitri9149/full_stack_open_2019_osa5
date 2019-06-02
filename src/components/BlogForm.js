d44db63a2953a7864a13d52f4302f7d5672960c9
Dmitri <34885758+Dmitri9149@users.noreply.github.com>
2019-06-02 20:57:39 +0300

    Toggle funtionality is added (partly)

---
+++ src/components/BlogForm.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const BlogForm = ({
+  addBlog, 
+  newTitle, setNewTitle,
+  newAuthor, setNewAuthor, 
+  newUrl, setNewUrl
+ }) => {
+  return(
+    <div>
+      <form onSubmit={addBlog}>
+        <div>
+          title
+          <input
+            type = 'text'
+            value={newTitle}
+            name= 'newtitle'
+            onChange={({ target }) => setNewTitle(target.value)}
+          />
+        </div>
+        <div>
+          author
+          <input
+            value={newAuthor}
+            onChange={({ target }) => setNewAuthor(target.value)}
+          />
+        </div>
+        <div>
+          url
+          <input
+            value={newUrl}
+            onChange={({ target }) => setNewUrl(target.value)}
+          />
+        </div>
+
+        <button type="submit">create</button>
+      </form>
+    </div>
+)}
+  
+  export default BlogForm
