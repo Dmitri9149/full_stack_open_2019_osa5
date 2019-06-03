93b5d5643aab57510a6d190fd85e5ee443e301ab
Dmitri <34885758+Dmitri9149@users.noreply.github.com>
2019-06-03 09:59:37 +0300

    new style for Blog

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,37 @@
 import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div onClick={() => console.log('clicked')}>
+        {blog.title} {blog.author}
+      </div>
+
+      <div>
+        {blog.author}
+      </div>
+
+      <div>
+        <p>
+          {blog.likes}
+          &emsp;
+          likes
+          &emsp;
+          <button type="submit">
+            like
+          </button>
+        </p>
+      </div>
+
   </div>
-)
+)}
 
 export default Blog
