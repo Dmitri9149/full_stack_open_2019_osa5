c7ee060b302684365488f596ba0d9cdba7725405
Dmitri <34885758+Dmitri9149@users.noreply.github.com>
2019-06-08 21:04:24 +0300

    5.16 ,but can not managed to elim act(....) warnings

--- src/App.test.js
+++ src/App.test.js
@@ -1,22 +1,27 @@
 import React from 'react'
 import {
-  render, waitForElement
+  render, waitForElement, act, cleanup
 } from '@testing-library/react'
 jest.mock('./services/blogs')
 import App from './App'
 
+afterEach(cleanup)
+
 describe('<App />', () => {
-  it('if no user logged, notes are not rendered', async () => {
+  it('if no user logged, blogs are not rendered', async () => {
     const component = render(
       <App />
     )
+
     component.rerender(<App />)
+
 
     await waitForElement(
       () => component.getByText('kirjaudu')
     )
 
     const blogs = component.container.querySelectorAll('.blog')
+
     expect(blogs.length).toBe(0)
   })
 })
